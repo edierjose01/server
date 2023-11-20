bb22a5a5a8efd11295f85f1206ba2bdaf79655a9
edierjose02 <edierjose01@gmail.com>
2023-11-19 20:00:19 -0500

    primer commit

---
+++ src/routes/product.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { deleteProduct, getProduct, getProducts, postProduct, updateProduct } from '../controllers/product';
+import validateToken from './validate-token';
+const router = Router();
+
+router.get('/', validateToken, getProducts)
+router.get('/:id', getProduct);
+router.delete('/:id', deleteProduct);
+router.post('/', postProduct);
+router.put('/:id', updateProduct);
+
+export default router;
